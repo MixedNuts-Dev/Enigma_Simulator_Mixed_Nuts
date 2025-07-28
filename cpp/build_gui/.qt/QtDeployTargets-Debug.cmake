fd72c7c56042e69fc9f71c32d6e02bcb5b5c3dd2
tukasa27 <tukasa.skyline@outlook.com>
2025-07-29 01:44:48 +0900

    Add build directories to gitignore

--- cpp/build_gui/.qt/QtDeployTargets-Debug.cmake
+++
@@ -1,6 +0,0 @@
-set(__QT_DEPLOY_TARGET_enigma_console_cpp_FILE D:/develop/Enigma_demo/cpp/build_gui/Debug/EnigmaSimulatorCpp.exe)
-set(__QT_DEPLOY_TARGET_enigma_console_cpp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_enigma_console_cpp_RUNTIME_DLLS )
-set(__QT_DEPLOY_TARGET_enigma_gui_cpp_FILE D:/develop/Enigma_demo/cpp/build_gui/Debug/EnigmaSimulatorCppGUI.exe)
-set(__QT_DEPLOY_TARGET_enigma_gui_cpp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_enigma_gui_cpp_RUNTIME_DLLS C:/Qt/6.9.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Cored.dll)
--- cpp/build_gui/.qt/QtDeployTargets-Release.cmake
+++
@@ -1,6 +0,0 @@
-set(__QT_DEPLOY_TARGET_enigma_console_cpp_FILE D:/develop/Enigma_demo/cpp/build_gui/Release/EnigmaSimulatorCpp.exe)
-set(__QT_DEPLOY_TARGET_enigma_console_cpp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_enigma_console_cpp_RUNTIME_DLLS )
-set(__QT_DEPLOY_TARGET_enigma_gui_cpp_FILE D:/develop/Enigma_demo/cpp/build_gui/Release/EnigmaSimulatorCppGUI.exe)
-set(__QT_DEPLOY_TARGET_enigma_gui_cpp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_enigma_gui_cpp_RUNTIME_DLLS C:/Qt/6.9.1/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Core.dll)
